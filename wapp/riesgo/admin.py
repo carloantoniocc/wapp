d6ca90502f9d3e0cb9dd6b0b9d872c33304efbfb
carlos <carloantoniocc@gmail.com>
2023-02-07 00:57:35 -0300

    modelos iniciales

--- wapp/riesgo/admin.py
+++ wapp/riesgo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Stock
+from .models import Broker
+from .models import Risk
+from .models import Probability
+from .models import Investment
+from .models import Briefcase
 
 # Register your models here.
+
+admin.site.register(Stock)
+admin.site.register(Broker)
+admin.site.register(Risk)
+admin.site.register(Probability)
+admin.site.register(Investment)
+admin.site.register(Briefcase)
--- wapp/riesgo/models.py
+++ wapp/riesgo/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class Broker(models.Model): 
+    name = models.CharField(max_length=200)
+    comision = models.IntegerField()
+    porc_entrada = models.IntegerField()
+
+    def __str__(self):
+        return  self.name
+
+class Stock(models.Model):
+    name = models.CharField(max_length=200)
+    cantidad = models.IntegerField()
+    valor = models.IntegerField()
+
+    def __str__(self):
+        return  self.name
+
+class Risk(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return  self.name
+
+class Probability(models.Model):
+    name = models.CharField(max_length=200)
+    valor: models.IntegerField()
+
+    def __str__(self):
+        return  self.name
+
+class Investment(models.Model):
+    stock_id = models.ForeignKey(Stock, on_delete= models.CASCADE)
+    broker_id = models.ForeignKey(Broker, on_delete= models.CASCADE)
+
+
+class Briefcase(models.Model):
+    name = models.CharField(max_length=200)
+    investment_id = models.ForeignKey(Investment, on_delete= models.CASCADE)
+    risk_id = models.ForeignKey(Risk, on_delete= models.CASCADE)
+    probability_id = models.ForeignKey(Probability, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return  self.name
+
+    
+
