2a5f3b94258efc1cf26d7ac1c1ef9632917ab900
Carlos Cornejo <carloantoniocc@gmail.com>
2023-02-07 16:46:22 -0300

    creacion de index

--- wapp/riesgo/views.py
+++ wapp/riesgo/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello word")
+    return render(request, "riesgo/index.html")
+    
+def portafolio(request):
+    return HttpResponse("portafolio")
+    
     
