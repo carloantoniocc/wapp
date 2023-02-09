d0484e5ca2af65ccb4b0f99f1416e2931209a08c
Carlos Cornejo <carloantoniocc@gmail.com>
2023-02-09 16:22:05 -0300

    inner join orm

--- wapp/riesgo/views.py
+++ wapp/riesgo/views.py
@@ -1,21 +1,27 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
 
 from .models import Broker, Stock
 
 def index(request):
-    brokers = Broker.objects.all()
-    stocks = Stock.objects.all()
-    todos = Stock.objects.select_related('brokers').all()
+    # brokers = Broker.objects.all()
+    # stocks = Stock.objects.all()
+    stocks = Stock.objects.select_related('broker_id').all()
+
+    print(stocks.query)
+    # print("ssss", Stock.objects.select_related('broker_id').all().query)
+        
+    for stock in stocks:
+        print(f"Nombre: {stock.name}, id: {stock.id},id: {stock.name}")
+        
+
     return render(request, "riesgo/index.html",{
-        "brokers": brokers,
-        "stocks": stocks,
-        "todos" : todos,
+        "stocks" : stocks,
     })
     
-    for broker in brokers:
-        print(broker.name)
-    
+
     
     
 def portafolio(request):
